c896385e59867814ad92ffdb073fc1ae1a41426c
Oliveira, Ronélio <66467630@galp.com>
2023-02-01 10:35:55 -0300

    update values

--- src/ios/GalpBipDriveImpl.swift
+++ src/ios/GalpBipDriveImpl.swift
@@ -7,7 +7,13 @@ import BipDriveCore
       var pluginResult = CDVPluginResult(
           status: CDVCommandStatus_OK
       )
-            
+
+      let filePath = Bundle.main.path(forResource: "Root", ofType: "plist")!
+      let plist = NSDictionary(contentsOfFile: filePath)
+      let BDriveAppId = plist?.object(forKey: "BDriveAppId")
+      let BDriveAppKey = plist?.object(forKey: "BDriveAppKey")
+      let BDriveAppClient = plist?.object(forKey: "BDriveAppClient")
+
       if(command.methodName == "openSDK") {
         let vc = BDParkingmetersAssembler.setUp(credentials: config, resultsDelegate: self, language: "es")
         self.present(vc, animated: true, completion: nil)
